d6523e3217362ebd7997f2af08c37bebae164d0c
Paraplux <theparaplux@gmail.com>
2022-02-19 01:47:43 +0100

    chore: switch to viteJs migrate all files from js to ts and jsx to tsx delete unsed files add tsconfig

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Router from "./routes/Router";
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+	colors: {},
+});
+
+ReactDOM.render(
+	<React.StrictMode>
+		<ChakraProvider theme={theme}>
+			<BrowserRouter>
+				<Router />
+			</BrowserRouter>
+		</ChakraProvider>
+	</React.StrictMode>,
+	document.getElementById("root"),
+);
---
+++ src/modules/navigation/Navigation.style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const NavigationWrapper = styled.div`
+	height: 70px;
+	padding: 0 16px;
+
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+
+	background: #f0e6ef;
+`;

0121b768f80c711758dfd3e9bceed2aaa63e5999
Paraplux <43264146+Paraplux@users.noreply.github.com>
2022-02-19 17:31:47 +0100

    feat: ✨ add aliases to viteJs and tsconfig (#4)

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from '../Layout';
-import Login from '../modules/authentication/screens/Login';
+import Login from 'modules/authentication/screens/Login';
 
 const About = () => <div>About</div>;
 
@@ -8,7 +8,7 @@ function Router() {
 	return (
 		<Routes>
 			<Route path="/" element={<Layout />}>
-				<Route path="" element={<h1>Home page</h1>} />
+				<Route path="" element={<h1>This is the home page</h1>} />
 				<Route path="login" element={<Login />} />
 				<Route path="about" element={<About />} />
 			</Route>

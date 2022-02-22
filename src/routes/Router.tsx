1f841b4276768b3d13c6d9df92d0b7f6ea2000e1
Paraplux <theparaplux@gmail.com>
2022-02-22 06:49:47 +0100

    feat(authentication): ✨ add login screen

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,6 +2,7 @@ import { Heading } from '@chakra-ui/react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from '../Layout';
 import Login from 'modules/authentication/screens/Login/Login';
+import TestAPI from 'modules/authentication/screens/TestAPI';
 
 function Router() {
 	return (
@@ -9,6 +10,7 @@ function Router() {
 			<Route path="/" element={<Layout />}>
 				<Route path="" element={<Heading>Home page</Heading>} />
 				<Route path="login" element={<Login />} />
+				<Route path="ping" element={<TestAPI />} />
 			</Route>
 		</Routes>
 	);

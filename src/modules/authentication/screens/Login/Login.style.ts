d227b5820e43d4df6399358ef39794b5b9953e36
Paraplux <theparaplux@gmail.com>
2022-02-22 06:49:47 +0100

    feat(authentication): ✨ add login screen

---
+++ src/modules/authentication/screens/Login/Login.style.ts
@@ -0,0 +1,20 @@
+import { Box } from '@chakra-ui/react';
+import styled from 'styled-components';
+
+export const LoginWrapper = styled(Box)`
+	width: 40%;
+	margin-left: 25%;
+	margin-top: 10%;
+	padding: 64px 32px;
+	text-align: center;
+	/* border-radius: 16px; */
+	/* box-shadow: 0 0 8px rgba(0, 0, 0, 0.2); */
+`;
+export const LoginForm = styled.div`
+	margin-top: 64px;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	justify-content: center;
+	gap: 32px;
+`;
